4ba3a59a2528d682e3edc4927fff8e41209eca88
Nikesh Maharjan <nikeshmhr@gmail.com>
2019-03-07 01:15:33 +0545

    * Add config for deployment. * Fetch api url from config.

--- src/apis/todoapi.js
+++ src/apis/todoapi.js
@@ -1,5 +1,11 @@
 import axios from "axios";
+import config from "../config/config";
+
+let baseURL = config["development"].apiURL;
+if (process.env.NODE_ENV === "production") {
+	baseURL = config[process.env.NODE_ENV].apiURL;
+}
 
 export default axios.create({
-	baseURL: "http://localhost:9999"
+	baseURL
 });
