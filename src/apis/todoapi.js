86f649bcbba3a3b641ebe0a6d8452e1de9529ba7
Nikesh Maharjan <nikeshmhr@gmail.com>
2019-03-07 01:29:05 +0545

    * Log env vars.

--- src/apis/todoapi.js
+++ src/apis/todoapi.js
@@ -2,6 +2,7 @@ import axios from "axios";
 import config from "../config/config";
 
 let baseURL = config["development"].apiURL;
+console.log("env", process.env.NODE_ENV);
 if (process.env.NODE_ENV === "production") {
 	baseURL = config[process.env.NODE_ENV].apiURL;
 }
